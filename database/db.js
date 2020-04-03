87c24b98dea328b4cb7a025247fee583a0b5622e
Mykhailo Herasimov <mogerasimov@gmail.com>
2020-04-03 14:50:55 -0300

    minor changes

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: //'mongodb+srv://ujwal:ujwal@cluster0-hj8l4.mongodb.net/mydb?retryWrites=true&w=majority'
-        'mongodb+srv://mherasimov:Gerasimov996@cluster0-zo6c5.mongodb.net/foodably?retryWrites=true&w=majority'
+  db:
+    'mongodb+srv://mherasimov:Gerasimov996@cluster0-zo6c5.mongodb.net/foodably?retryWrites=true&w=majority'
 };
