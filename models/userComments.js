05520c233245f34f505b8984d898dcb11574537e
Sneha Jayavardhini Doguparthi <sn536318@dal.ca>
2020-04-04 18:17:00 -0300

    Update userComments.js

--- models/userComments.js
+++ models/userComments.js
@@ -1,13 +1,10 @@
+//@author : SNEHA JAYAVARDHINI
 const mongoose = require('mongoose');
 const meanieMongoose = require('meanie-mongoose-to-json');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
     comment:String,
-    // userId:{
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'User'
-    // },
     user:{
         type: Schema.Types.ObjectId,
         ref: 'User'
