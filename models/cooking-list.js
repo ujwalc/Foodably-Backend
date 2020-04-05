fd9c05cde666483bbdfe3bc82292f53e8ca4ede6
SHASHANK REDDY MUPPIDI <sh375383@dal.ca>
2020-04-04 22:20:16 -0300

    Update cooking-list.js with comments

--- models/cooking-list.js
+++ models/cooking-list.js
@@ -4,7 +4,7 @@ const meanieMongoose = require('meanie-mongoose-to-json');
 const Schema = mongoose.Schema;
 
 const ingredient = require('./ingredient');
-
+//Schema to add ingredients list as shopping list with cooking id's
 const cookingSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
